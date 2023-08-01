322c8f13ebaa82fc8fa503abc96cc4675434cfa3
Daniel Silva Ramos <daniel.silvaramos.93@gmail.com>
2023-07-31 22:33:53 -0300

    renamed:    public/favicon.ico -> public/ico.ico 	deleted:    public/index.css 	modified:   public/index.html 	modified:   src/pages/login/index.tsx

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { useAuth } from "../../hooks/useAuth";
-import { Box, Stack, TextField, InputAdornment, IconButton, Button, Grid, Paper, Container } from "@mui/material";
+import { Box, Stack, TextField, InputAdornment, IconButton, Button, Grid, Paper, Container, CssBaseline } from "@mui/material";
 
 const Login = () => {
     const [word, setWord] = React.useState('')
@@ -17,49 +17,46 @@ const Login = () => {
 
     }
     return (
-        <Grid container style={{
-            // display: 'inline-flex',
-            // position: 'absolute',
-            // height: '100vh',
-
-        }}
-        >
-            {/* <Grid container style={{ position: 'absolute' }}> */}
-            <Grid container
-                direction="column"
-                justifyContent="center"
-                alignItems="center"
-                sx={{
-                    backgroundColor: 'goldenrod',
-                    // display: 'flex',
-                    // height: 'vw',
-                    // heig
-
-                }}
-            // spacing={5}
-            >
-                <TextField name="user" label="Username" />
-                <TextField
-                    name="password"
-                    label="Senha"
-                // type={showPassword ? 'text' : 'password'}
-                // InputProps={{
-                //     endAdornment: (
-                //         <InputAdornment position="end">
-                //             <IconButton onClick={() => alert('Hello!')} edge="end">
-                //                 <Iconify icon={true ? 'eva:eye-fill' : 'eva:eye-off-fill'} />
-                //             </IconButton>
-                //         </InputAdornment>
-                //     ),
-                // }}
-                />
-                <Button size="large" type="submit" variant="contained" onClick={handleClick}>
-                    Login
-                </Button>
-                {/* <Paper sx={{ width: 100, height: 100, backgroundColor: 'blue' }} /> */}
-            </Grid>
-            {/* </Grid> */}
-        </Grid>
+        <>
+            <CssBaseline />
+            <Container fixed >
+                <Grid
+                    container
+                    alignItems="center"
+                    justifyContent="center"
+                    sx={{ height: "100vh" }}
+                >
+                    <Paper elevation={12}>
+                        <Grid
+                            container
+                            direction="column"
+                            justifyContent="space-evenly"
+                            alignItems="center"
+                            sx={{
+                                minHeight: '250px',
+                                width: 400,
+                            }}>
+                            <TextField name="user" label="Usuário" />
+                            <TextField name="password" label="Senha"
+                            // type={showPassword ? 'text' : 'password'}
+                            // InputProps={{
+                            //     endAdornment: (
+                            //         <InputAdornment position="end">
+                            //             <IconButton onClick={() => alert('Hello!')} edge="end">
+                            //                 <Iconify icon={true ? 'eva:eye-fill' : 'eva:eye-off-fill'} />
+                            //             </IconButton>
+                            //         </InputAdornment>
+                            //     ),
+                            // }}
+                            />
+                            <Button size="large" type="submit" variant="contained" onClick={handleClick}>
+                                Login
+                            </Button>
+                        </Grid>
+                    </Paper>
+                </Grid>
+            </Container >
+        </>
     )
 }
 
