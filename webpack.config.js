b4639c1d5821546ba07bda9fdc89cebd3dc53740
Daniel Silva Ramos <daniel.silvaramos.93@gmail.com>
2024-11-18 10:34:38 -0300

    modified:   webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,6 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve( __dirname, 'dist' ),
-    // publicPath: '/',
     assetModuleFilename: 'images/[hash][ext][query]',
   },
   module: {
@@ -34,25 +33,12 @@ module.exports = {
     extensions: ['.ts', '.tsx', '.js'],
   },
   devServer: {
-    // headers: {
-    //   'Content-Security-Policy': "default-src 'self' https://jsonplaceholder.typicode.com; img-src *; script-src 'self' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; connect-src 'self' https://jsonplaceholder.typicode.com",
-    // },
     client: {
       logging: 'verbose',
     },
     open: true,
     hot: true,
     historyApiFallback: true,
-    // proxy: [
-    //   {
-    //     context: ['/api'],
-    //     target: 'http://127.0.0.1:5000',
-    //     pathRewrite: { '^/api': '' },
-    //     changeOrigin: true,
-    //     secure: false,
-    //     logLevel: 'debug',
-    //   }
-    // ]
   },
   plugins: [new HtmlWebpackPlugin( { template: './public/index.html' } )],
 };
